4853bd3be7cff5882c909404c95c623a55951e01
nobody <nobody>
2024-07-29 15:08:33 +0900

    Update BallControl.cs

--- Assets/Scripts/BallControl.cs
+++ Assets/Scripts/BallControl.cs
@@ -22,18 +22,28 @@ public class BallControl : MonoBehaviour
     // Update is called once per frame
     void Update()
     {
+        // C#에선 System.Random을 사용했지만 Unity에선 요걸 사용
+        int iv = Random.Range(1, 46); // iv에는 45까지만 들어온다
+        // 실수일때만 뒤에 값이 포함 된다는 것 주의!!!
+        float fv = Random.Range(0.1f, 1.0f); // fv에 1.0f 가 올 수 있다
+
         // ball reset
         if (Input.GetKeyUp(KeyCode.Space))
         {
             // ball의 transform.position 값을 변경해서 ball 위치를 reset (new Vector(-5,4,0))
-            MyObj.transform.position = new Vector3(-5.0f, 4f, 0f);
+            // x좌표는 -8.27f ~ 8.2f 사이에서 랜덤으로
+            fv = Random.Range(-8.27f, 8.2f);
+            MyObj.transform.position = new Vector3(fv, 4f, 0f);
 
             // 매번 누를 때마다 ballColor에 있는 값으로  Color도 바꿔보자
             sr.color = ballColor[ballIndex];
-            ballIndex++;
-            if (ballIndex >= ballColor.Length)
-                ballIndex = 0;
-
+            // 인덱스를 순서대로 넘기면서 ballColor 가져올 때
+            //ballIndex++;
+            //if (ballIndex >= ballColor.Length)
+            //    ballIndex = 0;
+            // 인덱스를 랜덤으로 가져와보자
+            // 0 <= ballIndex <= 5
+            ballIndex = Random.Range(0, 6);
         }
     }
 }
